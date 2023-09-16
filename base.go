b694dad7eddb9beea016e86315c78be734d3fe2f
杜涛 <duxirui1992@dingtalk.com>
2023-09-17 03:06:47 +0800

    初始化提交

---
+++ base.go
@@ -0,0 +1,54 @@
+package base
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// Response 统一返回结构
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+// CubeResponse 统一返回结构
+func CubeResponse(c *gin.Context, code int, message string, data any) {
+	c.JSON(http.StatusOK, Response{
+		Code:    code,
+		Message: message,
+		Data:    data,
+	})
+}
+
+func CubeResponseSuccess(c *gin.Context, obj any) {
+	c.JSON(http.StatusOK, Response{
+		Code:    200,
+		Message: "Success",
+		Data:    obj,
+	})
+}
+
+func CubeResponseError(c *gin.Context, code int, message string) {
+	c.JSON(code, Response{
+		Code:    code,
+		Message: message,
+		Data:    nil,
+	})
+}
+
+func CubeResponseErrorForServerErr(c *gin.Context, message string) {
+	c.JSON(http.StatusInternalServerError, Response{
+		Code:    http.StatusInternalServerError,
+		Message: message,
+		Data:    nil,
+	})
+}
+
+type CubeError struct {
+	Msg string
+}
+
+func (e *CubeError) Error() string {
+	return e.Msg
+}
